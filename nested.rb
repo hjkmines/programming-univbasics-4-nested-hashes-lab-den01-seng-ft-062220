d87dd2d64274a3b25f70b235e3e9588de13203a3
Hyung Kim <hjkmines@gmail.com>
2020-06-16 03:35:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+	# programmer_hash[:alan_kay][:known_for]
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -68,7 +68,7 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-
+# 
 	programmer_hash = 
  		{
       :grace_hopper => {
